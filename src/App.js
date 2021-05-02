48c5fb705cee87466e6754b1f454a4b521fb25c9
Will <28814921+wkey1980@users.noreply.github.com>
2021-05-02 01:43:06 +0100

    React Basic Boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <h2>This is learning Basic React</h2>
+        </div>
+    );
+}
+
+export default App
